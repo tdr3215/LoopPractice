d659b7078147cf062efe8a821a6cfa04a727dd2a
Tiffany Rose <tiffany.drose@gmail.com>
2022-06-21 13:52:04 -0400

    first commit

---
+++ src/LoopPractice.java
@@ -0,0 +1,83 @@
+import java.util.*;
+
+public class LoopPractice {
+	public static void main(String[] args) {
+//		Get 5 numbers from the user
+//		ArrayList<Integer> numbers = new ArrayList<Integer>(Arrays.asList());
+//
+//		Scanner input = new Scanner(System.in);
+//
+//		System.out.println("Give me a number");
+//		Integer firstNum = Integer.parseInt(input.nextLine());
+//		numbers.add(firstNum);
+//
+//		System.out.println("Give me another number");
+//		Integer scndNum = Integer.parseInt(input.nextLine());
+//		numbers.add(scndNum);
+//
+//		System.out.println("Give me another number");
+//		Integer thrdNum = Integer.parseInt(input.nextLine());
+//		numbers.add(thrdNum);
+//
+//		System.out.println("Give me another number");
+//		Integer fourthNum = Integer.parseInt(input.nextLine());
+//		numbers.add(fourthNum);
+//
+//		System.out.println("Give me one last number");
+//		Integer fifthNum = Integer.parseInt(input.nextLine());
+//		numbers.add(fifthNum);
+//
+////		loop over the array to find given properties
+//
+//		Integer sum = 0;
+//		Integer product = 1;
+//		Integer largest = numbers.get(0);
+//		Integer smallest = numbers.get(0);
+		
+//
+//		for (Integer num : numbers) {
+////			sum
+//			sum += num;
+////			product
+//			product *= num;
+////			largest
+//			if (num > largest) {
+//				largest = num;
+//			}
+////			smallest
+//
+//			if (num < smallest) {
+//				smallest = num;
+//			}
+//		}
+//
+//		System.out.printf("The sum is %s.\nThe product is %s.\nThe largest number is %s\nThe smallest number is %s.\n",
+//				sum, product, largest, smallest);
+		
+//		CAR 
+//		
+//		HashMap<String,String> vehicles = new HashMap<>();
+//		vehicles.put("Toyota", "Camry");
+//		vehicles.put("Honda", "Civic");
+//		vehicles.put("Hyundai", "Santa Fe");
+//		vehicles.put("Volkswagen", "Jetta");
+//		 
+//		Scanner input = new Scanner(System.in);
+//		System.out.println("What car are you looking for?");
+//		String car = input.nextLine();
+//		
+//		for(Map.Entry<String, String> entry : vehicles.entrySet()) {
+//			String make = entry.getKey();
+//			String model = entry.getValue();
+//			
+//			if (car.equals(model)) {
+//				System.out.printf("Oh, you're looking for our %s section? The %s selection is right over here", make,model);
+//			}
+//			
+//		}
+		
+	
+		
+		
+	}
+}
